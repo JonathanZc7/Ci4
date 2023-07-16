db2655ed8fa9780ecfd10d9edab8a44ef1c7e908
jonathanzc7 <jzambrano8069@utm.edu.ec>
2023-07-16 09:20:13 -0500

    Subida deber

---
+++ app/Controllers/Home.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controllers;
+
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    
+    }
+
+    public function Prueba()
+    {
+        echo 'Hello esto es una prueba';
+        
+     }
+    
+
+    public function Api()
+    {
+        $usuario = array
+        ( 
+        'usuario'=> 'pepe',
+        'Nombre'=> 'Jose Calderon',
+        'Ciudad'=> 'Manta',
+        'Direccion'=> 'metro',
+        'telefono'=> '098021233', 
+        );
+        
+        return $this->response->setJSON($usuario); 
+        
+        
+      } 
+      public function Prueba2()
+    {
+        $this->db=\Config\Database::connect();
+        
+        $query=$this->db->query("SELECT * FROM usuario");
+        $result=$query->getResult();
+        return $this->response->setJSON($result);
+
+        //echo "Hello esta conectada la BD tareadesarrollo en postgrest";
+
+     }
+}
