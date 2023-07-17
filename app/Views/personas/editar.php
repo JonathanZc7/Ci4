6b3e5f818dd8344ed29fcf2dc35d8bdf50f46b3d
jonathanzc7 <jzambrano8069@utm.edu.ec>
2023-07-16 21:03:43 -0500

    Act

--- app/Views/personas/editar.php
+++ app/Views/personas/editar.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?=$cabecera?>
+
     <title>Document Title</title>
 </head>
 <body>
     Formulario de editar
     
-</body>
-</html>
+    <?=$piepagina?>
